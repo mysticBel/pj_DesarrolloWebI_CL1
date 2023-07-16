86a8b3fbb669bc9ad4ae171d761fa5c683381cd3
mysticBel <dnxbel@gmail.com>
2023-07-16 03:24:42 -0500

    Agregar archivos de proyecto.

---
+++ DesarrolloWebI_CL1_MaribelMaza/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace DesarrolloWebI_CL1_MaribelMaza.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ DesarrolloWebI_CL1_MaribelMaza/Models/Factura.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Xml.Linq;
+
+namespace DesarrolloWebI_CL1_MaribelMaza.Models
+{
+    public class Factura
+    {
+        [Display(Name = "ID Factura")]
+        public int idFactura{ get; set; }
+
+        [Display(Name = "Fecha de Emision")]
+        public DateTime fechaEmision { get; set; }
+
+        [Display(Name = "Nombre")]
+        public string? nombreProducto { get; set; }
+
+        [Display(Name = "Precio Unitario")]
+        public decimal precioUnitario { get; set; }
+
+        [Display(Name = "Cantidad")]
+        public decimal cantidad { get; set; }
+
+        [Display(Name = "Monto")]
+        public decimal monto { get; set; }
+    }
+}
