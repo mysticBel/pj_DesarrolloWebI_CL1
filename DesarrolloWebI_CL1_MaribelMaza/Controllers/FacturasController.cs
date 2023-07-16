ab130242ff19094b39a8ddef737acbf45386e83c
mysticBel <dnxbel@gmail.com>
2023-07-16 04:57:12 -0500

    action y vista GetFacturasPorProducto y paginacion

---
+++ DesarrolloWebI_CL1_MaribelMaza/Controllers/FacturasController.cs
@@ -0,0 +1,80 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+//para la cadena de conexion
+using Microsoft.Extensions.Configuration;
+//
+using Microsoft.Data.SqlClient;
+using DesarrolloWebI_CL1_MaribelMaza.Models;
+//
+using System.Data; //para los sp
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace DesarrolloWebI_CL1_MaribelMaza.Controllers
+{
+    public class FacturasController : Controller
+    {
+
+        public readonly IConfiguration _config;
+        public string cadena;
+
+        public FacturasController(IConfiguration _config)
+        {
+            this._config = _config;  
+            this.cadena = _config["ConnectionStrings:connection"]; 
+        }
+
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+        IEnumerable<Factura> GetFacturasPorProducto(string nombreProducto)
+        {
+            List<Factura> facturas = new List<Factura>();
+
+            using (SqlConnection connection = new SqlConnection(this.cadena))
+            {
+                SqlCommand cmd = new SqlCommand("sp_GetFacturasPorProducto", connection);
+                cmd.CommandType = CommandType.StoredProcedure;
+                cmd.Parameters.AddWithValue("@prmstrNombreProducto", nombreProducto);
+
+                connection.Open();
+                SqlDataReader dr = cmd.ExecuteReader();
+
+                while (dr.Read())
+                {
+                    facturas.Add(new Factura()
+                    {
+                        idFactura = dr.GetInt32(0),
+                        fechaEmision = dr.GetDateTime(1),
+                        nombreProducto = dr.GetString(2),
+                        precioUnitario = dr.GetDecimal(3),
+                        cantidad = dr.GetDecimal(4),
+                        monto= dr.GetDecimal(5),
+                    });
+                }
+            }
+            return facturas;
+        }
+
+        public async Task<IActionResult> GetFacturasPorProducto(string nombreProducto = "", int pagina = 0)
+        {
+            //validacion por si no se envia nada
+            if (nombreProducto == null)
+                nombreProducto = "";
+
+            IEnumerable<Factura> facturas = GetFacturasPorProducto(nombreProducto);
+            int filasPagina = 5;  // indico que tome 5 items por pagina
+            int totalFilas = facturas.Count();
+            int numeroPaginas = totalFilas % filasPagina == 0 ? totalFilas / filasPagina : (totalFilas / filasPagina) + 1;
+
+            ViewBag.pagina = pagina;
+            ViewBag.numeroPaginas = numeroPaginas;
+            ViewBag.nombreProducto = nombreProducto;
+
+            return View(await Task.Run(() => facturas.Skip(pagina * filasPagina).Take(filasPagina)));
+
+        }
+
+    }
+}
